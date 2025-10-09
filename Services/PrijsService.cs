20bcea6da53be8368792d32b68c7507e8bb0419a
Michiel Wouters <michiel_woute64@hotmail.com>
2025-10-09 14:54:20 +0200

    Unit testing

---
+++ Services/PrijsService.cs
@@ -0,0 +1,16 @@
+﻿namespace WebAPIDemo.Services
+{
+    public class PrijsService
+    {
+        public decimal BerekenTotaalPrijs(decimal prijs, decimal btw)
+        {
+            decimal prijsMetBtw = prijs + prijs * btw;
+            if (prijs > 1000)
+            {
+                return Math.Round(prijsMetBtw * 0.9m, 2);
+            }
+
+            return Math.Round(prijsMetBtw, 2);
+        }
+    }
+}
